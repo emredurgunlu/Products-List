451e02fb1e9fc169f808e3011a37d1d4ee5a6bae
emre durgunlu <emredurgunlu@gmail.com>
2023-12-04 00:17:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./components/header/Header";
+import Products from "./components/products/ProductsList";
+import { products, categories } from "./helper/data";
+import "./App.scss";
+import HeaderH from "./components/headerHam/HeaderH";
 
 function App() {
+  console.log(products);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header categories={categories} text="Products List" /> */}
+      <HeaderH categories={categories} />
+      <Products products={products} />
     </div>
   );
 }
